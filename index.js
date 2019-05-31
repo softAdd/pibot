ef1e4503be4254729147b7e4b51dc453bbbe035e
Maksim Konkov <soft.snake867@gmail.com>
2019-05-31 23:31:53 +0500

    add index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+// const customParser = require('./js/custom_parser');
+
+const { TOKEN } = process.env;
+const { PROXY } = process.env;
+
+const bot = new TelegramBot(TOKEN, {
+  polling: true,
+  baseApiUrl: `https://${PROXY}/api.telegram.org`
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
