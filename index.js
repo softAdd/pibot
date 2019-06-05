fa5dbcda6b0344036d9c03d72aca8d6f6500166a
Maksim Konkov <soft.snake867@gmail.com>
2019-06-05 21:35:28 +0500

    update index.js for new code

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const moment = require('moment');
 
 const parser = new ParserModule();
 
+setInterval(function() {
+  if (moment().format('H') === '6') {
+    // send data to subscribers
+  }
+}, 60 * 60 * 1000)
 
 const { TOKEN } = process.env;
 const { PROXY } = process.env;
@@ -14,32 +19,32 @@ const bot = new TelegramBot(TOKEN, {
   baseApiUrl: `https://${PROXY}/api.telegram.org`
 });
 
+const messenger = {
+  '/weather': async function() {
+    // code
+  },
+  weather: async function() {
+    await parser.openBrowser();
+    const message = 'Погода: ' +  await parser.weatherNow();
+    // bot.sendMessage(chatId, message);
+    await parser.closeBrowser();
+  },
+  full: async function() {
+    await parser.openBrowser();
+    const weather = await parser.weatherNow();
+    const btc = await parser.bitcoinToDollar();
+    const dol = await parser.dollarToRub();
+    const message = 'Сегодня: ' + moment().format('LLLL') + 
+    '\n\nПогода: ' + weather + '\n\n' + btc + '\n' + dol;
+    // bot.sendMessage(chatId, message);
+    await parser.closeBrowser();
+  },
+  sub: async function() {
+    // code
+  }
+}
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
-  if (msg.text === '/weather') {
-    (async function() {
-      await parser.openBrowser();
-      const message = 'Погода: ' +  await parser.weatherNow();
-      bot.sendMessage(chatId, message);
-      await parser.closeBrowser();
-    })();
-  }
-
-  if (msg.text === '/full') {
-    (async function() {
-      await parser.openBrowser();
-      const weather = await parser.weatherNow();
-      const btc = await parser.bitcoinToDollar();
-      const dol = await parser.dollarToRub();
-      const message = 'Сегодня: ' + moment().format('LLLL') + 
-      '\n\nПогода: ' + weather + '\n\n' + btc + '\n' + dol;
-      bot.sendMessage(chatId, message);
-      await parser.closeBrowser();
-    })();
-  }
-
-  if (msg.text === '/sub') {
-    // code...
-  }
+  // use messenger[message] here
 });
