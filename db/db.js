4ecfd79d496c82842fb83a1b75067646046ce2de
Maksim Konkov <soft.snake867@gmail.com>
2019-06-11 23:48:14 +0500

    new struct and JSON db in addition to mongo

--- db/db.js
+++ db/db.js
@@ -1,4 +1,9 @@
+require('dotenv').config({ path: '../dev.env'});
 const mongoose = require('mongoose');
+const moment = require('moment');
+const fs = require('fs');
+const ParserModule = require('../js/custom_parser');
+const parser = new ParserModule();
 
 const { CONNECT_STRING } = process.env;
 
@@ -15,4 +20,56 @@ mongoose.connect(CONNECT_STRING, {
     useNewUrlParser: true
 })
 
-module.exports = mongoose
+async function getWeather() {
+    await parser.openBrowser();
+    const message = 'Погода: ' + await parser.weatherNow();
+    await parser.closeBrowser();
+    return message
+}
+
+async function getFull() {
+    await parser.openBrowser();
+    const weather = await parser.weatherNow();
+    const btc = await parser.bitcoinToDollar();
+    const dol = await parser.dollarToRub();
+    const message = 'Сегодня: ' + moment().format('LLLL') +
+        '\n\nПогода: ' + weather + '\n\n' + btc + '\n' + dol;
+    await parser.closeBrowser();
+    return message;
+}
+
+const Doc = require('./models/Info');
+
+/* insert new documents in db periodically */
+
+// mongodb, full info, every 30 mins
+setInterval(async function () {
+    const doc = new Doc({
+        text: await getFull()
+    });
+    doc.save();
+}, 30 * 60 * 1000)
+
+// json file, weather, every 10 mins
+setInterval(async function () {
+    const doc = {
+        weather: await getWeather(),
+        time: `Время обновления: ${moment().format('LT')}`
+    }
+    fs.writeFile('current_weather.json', JSON.stringify(doc), function(err) {
+        if (err) throw new Error('Возникла ошибка при записи данных в JSON файл.');
+    })
+}, 0.1 * 60 * 1000)
+
+function getJSONdata() {
+    fs.readFile('current_weather.json', 'utf-8', function(err, result) {
+        if (err) throw new Error('Ошибка при чтении файла');
+        return JSON.parse(result);
+    })
+}
+
+const data = {
+    // 
+}
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require('dotenv').config({ path: 'dev.env'});
 const TelegramBot = require('node-telegram-bot-api');
-const ParserModule = require('./js/custom_parser');
 const moment = require('moment');
-
-const parser = new ParserModule();
 
 const { TOKEN } = process.env;
 const { PROXY } = process.env;
@@ -13,26 +10,8 @@ const bot = new TelegramBot(TOKEN, {
   baseApiUrl: `https://${PROXY}/api.telegram.org`
 });
 
-async function getWeather() {
-  await parser.openBrowser();
-  const message = 'Погода: ' +  await parser.weatherNow();
-  await parser.closeBrowser();
-  return message
-}
-
-async function getFull() {
-  await parser.openBrowser();
-  const weather = await parser.weatherNow();
-  const btc = await parser.bitcoinToDollar();
-  const dol = await parser.dollarToRub();
-  const message = 'Сегодня: ' + moment().format('LLLL') + 
-  '\n\nПогода: ' + weather + '\n\n' + btc + '\n' + dol;
-  await parser.closeBrowser();
-  return message;
-}
-
 // const mongoose = require('./db/db');
-const Doc = require('./db/models/Info');
+
 const Sub = require('./db/models/Sub.js');
 
 // send data to subscribers
@@ -42,13 +21,6 @@ setInterval(function() {
   }
 }, 60 * 60 * 1000)
 
-// insert new documents in mongo every 30 mins
-setInterval(function() {
-  const doc = new Doc({
-    text: getFull()
-  });
-  doc.save();
-}, 60 * 30 * 1000)
 
 const messenger = {
   '/weather': async function() {
